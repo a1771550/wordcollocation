97e4ee0c34c0c265a4b7b4883200420a73cbf484
Kevin Lau <a1771550@gmail.com>
2014-09-12 22:04:24 +0800

    update sqlscripts

--- sqlscripts/dbo.WcRoles.create.sql
+++ sqlscripts/dbo.WcRoles.create.sql
@@ -2,7 +2,7 @@
     [Id]         INT           IDENTITY (1, 1) NOT NULL,
     [Name]       NVARCHAR (20) NOT NULL,
     [RowVersion] ROWVERSION    NOT NULL,
-    [CanDel] BIT NOT NULL DEFAULT 1, 
+    [CanDel] BIT NOT NULL DEFAULT 0, 
     CONSTRAINT [PK_WcRoles] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
