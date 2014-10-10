c18379bb7a5d302847111a96ec1ab767c037b0fa
Kevin Lau <a1771550@gmail.com>
2014-10-11 00:26:15 +0800

    bundles all progress-related js & css files and add qtip-related js and css files

--- UI/App_Start/BundleConfig.cs
+++ UI/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace UI
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/layout").Include("~/js/jquery-{version}.js").Include("~/js/bootstrap.js").Include("~/js/tinynav.js").Include("~/js/template.js").Include("~/js/socialLinks.js").Include("~/js/jquery-cookie-plugin.js").Include("~/js/timezone.js").Include("~/js/jquery-ui.custom/jquery-ui.js"));
+			bundles.Add(new ScriptBundle("~/bundles/layout").Include("~/js/jquery-{version}.js").Include("~/js/bootstrap.js").Include("~/js/tinynav.js").Include("~/js/template.js").Include("~/js/socialLinks.js").Include("~/js/jquery-cookie-plugin.js").Include("~/js/timezone.js").Include("~/js/jquery-ui.custom/jquery-ui.js").Include("~/js/searchbox_submit.js"));
 
 			//bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/js/modernizr.js"));
 
@@ -36,7 +36,7 @@ namespace UI
 			//@import url("/css/wisewords/halflings.css");
 			//@import url("/css/wisewords/template.css");
 			//@import url("/css/wisewords/colors/color-classic.css");
-			bundles.Add(new StyleBundle("~/bundles/css").Include("~/css/wisewords/magic-bootstrap.css").Include("~/css/wisewords/bootstrap.css").Include("~/css/wisewords/bootstrap-responsive.css").Include("~/css/wisewords/socialicons.css").Include("~/css/wisewords/glyphicons.css").Include("~/css/wisewords/halflings.css").Include("~/css/wisewords/template.css").Include("~/css/wisewords/colors/color-classic.css").Include("~/css/site.css").Include("~/css/adminLinks.css").Include("~/js/jquery-ui.custom/jquery-ui.css").Include("~/js/jquery-ui.custom/jquery-ui.structure.css").Include("~/js/jquery-ui.custom/jquery-ui.theme.css"));
+			bundles.Add(new StyleBundle("~/bundles/css").Include("~/css/wisewords/magic-bootstrap.css").Include("~/css/wisewords/bootstrap.css").Include("~/css/wisewords/bootstrap-responsive.css").Include("~/css/wisewords/socialicons.css").Include("~/css/wisewords/glyphicons.css").Include("~/css/wisewords/halflings.css").Include("~/css/wisewords/template.css").Include("~/css/wisewords/colors/color-classic.css").Include("~/css/site.css").Include("~/css/adminLinks.css").Include("~/js/jquery-ui.custom/jquery-ui.css").Include("~/js/jquery-ui.custom/jquery-ui.structure.css").Include("~/js/jquery-ui.custom/jquery-ui.theme.css").Include("~/css/progress.css"));
 
 			// Set EnableOptimizations to false for debugging. For more information,
 			// visit http://go.microsoft.com/fwlink/?LinkId=301862
